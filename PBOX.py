684850f8475a30c691c755cea5b968e9078f7339
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-10-11 13:50:28 +0100

    Incremented version number

--- PBOX.py
+++ PBOX.py
@@ -7,7 +7,7 @@ from time import sleep
 data = {
     "meta": {
         "name": "PBOX",
-        "ver": "0.1.0",
+        "ver": "0.2.0",
         "id": "6",
         "sentry": {
             "share_ip": True,  # Used to track unique cases of envountered errors
